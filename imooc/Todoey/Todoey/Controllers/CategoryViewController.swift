8463b8a400d0d47cb477c559cb4e0e2433c768b6
Aaron Guo <aaronguostudio@gmail.com>
2019-07-07 23:51:25 -0600

    add SwipeCellKit for todoey app

--- imooc/Todoey/Todoey/Controllers/CategoryViewController.swift
+++ imooc/Todoey/Todoey/Controllers/CategoryViewController.swift
@@ -21,6 +21,8 @@ class CategoryViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategories()
+        
+        tableView.rowHeight = 80.0
     }
 
     @IBAction func addButtonPressed(_ sender: Any) {
@@ -48,12 +50,13 @@ class CategoryViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    //MARK - TableView Datasource Methods
+    //MARK: - TableView Datasource Methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
         
         // let item = categoryArray[indexPath.row]
         cell.textLabel?.text = categoryArray?[indexPath.row].name ?? "No category yet"
+        cell.delegate = self
         
         return cell
     }
@@ -75,10 +78,10 @@ class CategoryViewController: UITableViewController {
         }
     }
     
-    //MARK - add new item
+    //MARK: - add new item
     
     
-    //MARK - save item
+    //MARK: - save item
     func saveCategory (category : Category) {
         do {
             try realm.write {
@@ -91,11 +94,47 @@ class CategoryViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    //MARK - load item
+    //MARK: - load item
     func loadCategories () {
         categoryArray = realm.objects(Category.self)
         
         tableView.reloadData()
     }
     
+}
+
+//MARK: - Swipe cell
+extension CategoryViewController : SwipeTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            
+            if let categoryForDeletion = self.categoryArray?[indexPath.row] {
+                do {
+                    try self.realm.write {
+                        self.realm.delete(categoryForDeletion)
+                    }
+                } catch {
+                    print("Category deletion failed")
+                }
+            }
+            
+            tableView.reloadData()
+           
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    // not works
+//    func collectionView(_ collectionView: UITableView, editActionsOptionsForItemAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+//        var options = SwipeOptions()
+//        options.expansionStyle = .destructive
+//        options.transitionStyle = .border
+//        return options
+//    }
 }
